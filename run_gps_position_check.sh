e8f41a2d1dbf168f62922885940fe6f4b6d1ace0
Kosaido <69334945+Kosaido@users.noreply.github.com>
2026-09-25 17:52:34 +0700

    Update run_gps_position_check.sh

--- run_gps_position_check.sh
+++ run_gps_position_check.sh
@@ -3,5 +3,5 @@
 # for THIS drone. EDIT the paths and --own/--topic flags below to
 # match your actual setup.
 source /opt/ros/humble/setup.bash
-source /home/pi/ros2_ws/install/setup.bash
-exec python3 /home/pi/gps_position_check.py --own 0 --topic /fmu/out/vehicle_global_position
+source /home/car/ros2_ws/install/setup.bash
+exec python3 /home/car/gps_position_check.py --own 0 --topic /fmu/out/vehicle_global_position
